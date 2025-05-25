36d4ca82dfe347ffe990b2cb89410456cdc0fadc
Bhanu pratap singh <157827402+MrBhanuPratapSingh@users.noreply.github.com>
2025-05-25 18:19:16 +0000

    comparsion

---
+++ 01_basic/04_comparsion.js
@@ -0,0 +1,15 @@
+// console.log("2">1);   output =True
+// console.log("02">1);   output =True
+
+// console.log(null>0);    false 
+// console.log(null==0);   false
+// console.log(null>=0);   true
+// console.log(null<=0);    true
+// console.log(null<0);      false
+
+// ===
+// console.log(2===2); true
+
+// console.log("2"===2); false because it also check the data type
+
+
