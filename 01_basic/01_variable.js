643b8f62ce6dd07ac1379c434e8e6742c1f20846
Bhanu pratap singh <157827402+MrBhanuPratapSingh@users.noreply.github.com>
2025-05-25 05:47:27 +0000

    done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId=12324           //for constant declartion use const
+let accountEmail="bps@gmail.com"  //for variable use let,var
+var accountPassword="1244"        //var don't use
+accountCity="ghaziabad"
+let accountState
+
+
+accountEmail="bhanu@gmail.com"
+accountPassword="9161"
+accountCity="rampur"
+// accountId=43  ->not allowed
+console.log(accountId); // for single print
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
