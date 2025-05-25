c620352c9f732c5ec7bdc54ee4ca3df0941854f2
Bhanu pratap singh <157827402+MrBhanuPratapSingh@users.noreply.github.com>
2025-05-25 13:34:39 +0000

    operation

--- 01_basic/01_conversionOperation.js
+++ 01_basic/01_conversionOperation.js
@@ -1,3 +1,4 @@
+/*
 let number=33
 console.log(typeof number); //number
 //or console.log(typeof (number)));
@@ -28,4 +29,65 @@ console.log(booleanIsLogged);
 //           ""=>false ,"fafdf"=>true
 
 //we can convert a number in string  By String
+*/
+ 
+
+//**************************************************operation***************************************** */
+let val=3;
+let negval=-val
+// console.log(negval); output -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2%2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**2);     ** use for power
+ 
+
+let str1="hello"
+let str2=" bhanu"
+let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2);      output=12
+// console.log(2+"1");      output=21
+// console.log("1"+ 2+3);   output=123
+// console.log(1+2+"1");    output=31
+
+// infix and postfix
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+//postfix
+let x1 = 3;
+const y1 = x1++;
+// x is 4; y is 3
+
+let x2 = 3n;
+const y2 = x2++;
+// x2 is 4n; y2 is 3n
+ 
+// prefix
+let a1 = 3;
+const b1 = ++a;
+// x is 4; y is 4
+
+let x3 = 3n;
+const y3 = ++x3;
+// x2 is 4n; y2 is 4n
+ 
+
+
+
+
+
 
