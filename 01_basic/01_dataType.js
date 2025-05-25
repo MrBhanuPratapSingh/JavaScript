c9e261c0ee96f501bf3b1e60c8f4c02eace652f4
Bhanu pratap singh <157827402+MrBhanuPratapSingh@users.noreply.github.com>
2025-05-25 06:35:17 +0000

    datatype

---
+++ 01_basic/01_dataType.js
@@ -0,0 +1,8 @@
+"use strict"; //treat all JS code as newer version
+ //alert(3+2)   not use because using Node.js not browser
+ console.log(3+3);
+ 
+ console.log(typeof undefined); //undefined
+ console.log(typeof null); // object
+ 
+ //symbol =>unique
