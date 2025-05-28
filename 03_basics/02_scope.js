911b56285888b5ab93129216d50259f10195b86b
Bhanu pratap singh <157827402+MrBhanuPratapSingh@users.noreply.github.com>
2025-05-28 09:26:01 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,9 @@
+console.log(addOne(5));// accessable out put 6
+
+function addOne(num){
+  return num+1
+}
+// addTwo(6)                      //Cannot access 'addTwo' before initialization
+const addTwo=function(num1){
+  return num1+2
+}
