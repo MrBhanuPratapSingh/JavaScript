9d8508296892d419a4bb984b588e36d64c3509c0
Bhanu pratap singh <157827402+MrBhanuPratapSingh@users.noreply.github.com>
2025-05-29 16:19:54 +0000

    falsy

---
+++ 04_control_flow/falsy.js
@@ -0,0 +1,31 @@
+// const val=""
+// if(val){
+//   console.log("value is");
+  
+// }else{
+//   console.log("not a value");
+  
+// }
+ //                             falsy value
+//  false,0,null,BigInt 0n,undefined,NaN,""
+
+   //                          truthy Value
+   // "0",'false',{},{},function(){}," ", 
+
+
+// Nullish Coalescing operator (??)
+let val1
+//val1 =10??15
+console.log(val1);  //10
+ let val2
+ val2=null??14
+//  console.log(val2); //14
+ 
+ val2=null??14??27
+ console.log(val2); //14
+
+ const icePrice=100
+ icePrice<=80 ?console.log("less than 80"):console.log("more than 80");
+ 
+ 
+ 
